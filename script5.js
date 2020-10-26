86405d9cd37d6e30ad85e65bfa2ffa582387f2ef
Kateryna Tsygankova <tsyg.kata@gmail.com>
2020-10-26 16:46:50 +0200

    add 3_6

---
+++ script5.js
@@ -0,0 +1,53 @@
+/*
+Суммирование значений свойства из массива объектов
+Напиши функцию calculateTotalPrice(allProdcuts, productName), 
+которая получает массив объектов и имя продукта (значение свойства name). 
+Возвращает общую стоимость продукта (цена умноженная на количество).
+
+Вызовы функции для проверки работоспособности твоей реализации.
+
+// calculateTotalPrice(products, 'Радар'));
+// 9080
+
+// calculateTotalPrice(products, 'Сканер')); // 10200
+
+// calculateTotalPrice(products, 'Захват')); // 2400
+
+// calculateTotalPrice(products, 'Дроид')); // 2800*/
+
+const calculateTotalPrice = function (array, prop){
+    let total = 0;
+for (const arrElement of array){
+    let values = Object.values (arrElement);
+    if (values[0] === prop){
+        total += values[1]*values[2]
+    }
+}
+
+return total
+}
+
+
+
+
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Радар', price: 1280, quantity: 2 },
+    { name: 'Радар', price: 1320, quantity: 1 },
+    { name: 'Сканер', price: 2700, quantity: 1 },
+    { name: 'Сканер', price: 2500, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 }
+]; 
+
+console.log(calculateTotalPrice(products, 'Радар'));  
+9080
+
+console.log(calculateTotalPrice(products, 'Сканер')); 
+10200
+
+console.log(calculateTotalPrice(products, 'Захват'));
+2400
+
+console.log(calculateTotalPrice(products, 'Дроид')); 
+2800
